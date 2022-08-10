38684b76ab76f5d217823c2692b7825febededa2
Samuel Raducan <samuel.thembp@gmail.com>
2022-08-10 11:00:58 +0100

    Install and setup rules for eslint

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const process = process.env.PORT;
 
 app.get('/', (req, res) => {
 	res.send('Hello chitters.');
 });
 
-const port = process.env.PORT || 3000;
+const port = process || 3000;
 app.listen(port, () =>
 	console.log(`Server is running on http://localhost:${port}`)
 );
