cbcc1752b00f9e3125ec671ab0533704d0096f82
Samuel Raducan <samuel.thembp@gmail.com>
2022-08-10 10:48:30 +0100

    Connect the app to the server

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+	res.send('Hello chitters.');
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
